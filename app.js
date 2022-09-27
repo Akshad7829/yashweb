64addf27b438295b9d2ea511a8571730bcd5921f
Akshad7829 <73118552+Akshad7829@users.noreply.github.com>
2022-09-27 23:36:13 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 const Message = require('./models/msg')
-
+const port = process.env.PORT || 3001;
 //express app
 
 
@@ -13,16 +13,23 @@ const app = express();
 
 
 
-//connect to mongodb
-const dbURI="mongodb+srv://yash:yash123456@cluster0.hnaib.mongodb.net/yash?retryWrites=true&w=majority"
+app.set('view engine', 'ejs');
+const dbURI="mongodb+srv://db:asn@7829@test.xtmowdy.mongodb.net/?retryWrites=true&w=majority";
+// const dbURI="mongodb+srv://<user>:yash123456@@cluster0.hnaib.mongodb.net/myFirstDatabase?authSource=admin&compressors=zlib&retryWrites=true&w=majority&ssl=true";
 mongoose.connect(dbURI,{ useNewUrlParser:true , useUnifiedTopology:true})
-.then((result) =>
-app.listen(process.env.PORT || 3000 ))
- .catch((err) => console.log(err))   
+try {
+    // Connect to the MongoDB cluster
+     mongoose.connect(
+      dbURI,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(" Mongoose is connected")
+    );
 
-//register view engine
+  } catch (e) {
+    console.log("could not connect");
+  }
 
-app.set('view engine','ejs');
+// app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 
@@ -79,3 +86,10 @@ app.get('/',function(req,res){
     res.render('index',{title:'Yash Dhanlobhe'})
 });
 
+app.set('port',(process.env.PORT || 3000));
+app.listen(app.get('port'),function(){
+  console.log('Running on port '+app.get('port'));
+});
+app.listen(port , ()=>{
+  console.log("connection on port " + port);
+})
